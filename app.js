dc051693179eb5eaf0f5a821710fc5b66ba429ed
chinonso.ebere <chinonso.ebere2017@gmail.com>
2021-02-23 20:57:47 +0400

    implemented salting and hashing for password

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const encrypt = require("mongoose-encryption")
+//password encryption strategy
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 
 
@@ -29,56 +30,71 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//check .env file for the SECRET variable
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
 app.route('/')
-.get((req,res)=>{
-  res.render("home");
-})
-.post((req,res)=>{
+  .get((req, res) => {
+    res.render("home");
+  })
+  .post((req, res) => {
 
-})
+  })
 
 app.route('/login')
-.get((req,res)=>{
-  res.render('login');
-})
-.post((req,res)=>{
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username},(err,foundUser)=>{
-    if(!err){
-      if(foundUser.password === password){
-        res.render('secrets');
-        console.log('User logged in')
+  .get((req, res) => {
+    res.render('login');
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password
+    User.findOne({
+      email: username
+    }, (err, foundUser) => {
+      if (!err) {
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, (err, result) => {
+            if (result === true) {
+              res.render('secrets');
+              console.log('User logged in')
+            } else {
+              console.log(err)
+            }
+
+          });
+        }
+      } else {
+        console.log(err)
       }
-    }else{
-      console.log(err)
-    }
-  });
-})
+    });
+  })
 
 app.route('/register')
-.get((req,res)=>{
-  res.render('register');
-})
-.post((req,res)=>{
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  .get((req, res) => {
+    res.render('register');
+  })
+  .post((req, res) => {
+    bcrypt.hash(req.body.password, saltRound, (err, hash) => {
+
+      if (!err) {
+        const newUser = new User({
+          email: req.body.username,
+          password: hash
+        });
+        newUser.save((err) => {
+          if (!err) {
+            res.render('secrets');
+            console.log('new user created')
+          }
+        });
+      } else {
+        console.log(err);
+      };
+
+    });
+
   });
 
-  newUser.save((err)=>{
-    if(!err){
-      res.render('secrets');
-      console.log('new user created')
-    }
-  })
-})
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Server started on port 3000")
 })
